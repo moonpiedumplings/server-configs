e4442e236f568d850d3faf92153006b5e00861ea
moonpiedumplings <jeff.fonseca777@gmail.com>
2024-01-19 15:36:50 -0800

    init

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz) { } }:
+
+let
+  my-python-packages = ps: with ps; [
+    ansible
+    ansible-core
+    molecule
+    molecule-plugins
+    # other python packages
+  ];
+in
+with pkgs;
+mkShell {
+  LC_ALL = "C.UTF-8";
+  LANG = "C.UTF-8";
+  ANSIBLE_HOST_KEY_CHECKING = "False";
+  buildInputs = [
+    # vagrant maybe for testing later
+    openssh
+    sshpass
+    sshs
+    (pkgs.python311.withPackages my-python-packages)
+    ansible-lint
+  ];
+}
